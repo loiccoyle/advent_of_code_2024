4c64ec1d1cb116e0b915bdbfafb720c11cf90170
Loic Coyle <loic.coyle@hotmail.fr>
2024-12-03 17:55:42 +0100

    add day3

---
+++ 3/solution.py
@@ -0,0 +1,23 @@
+import re
+
+INPUT = "input"
+
+
+PATTERN = re.compile(r"mul\((\d+),(\d+)\)")
+
+
+def compute(input: str) -> int:
+    return sum([int(match[0]) * int(match[1]) for match in re.findall(PATTERN, input)])
+
+
+if __name__ == "__main__":
+    with open(INPUT) as fp:
+        content = fp.read()
+    print("result", compute(content))
+
+    # part 2
+    disabled = re.compile(r"don't\(\)(.*?)(do\(\)|$)", re.DOTALL)
+    # remove the disabled parts
+    content = re.sub(disabled, "", content)
+    # same as above
+    print("part 2 result", compute(content))
